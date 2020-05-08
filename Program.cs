5c5468ad87c714d7b91c963f5fdac3df8083ddbd
Amber Volkmann <52728276+AmberVolkmann@users.noreply.github.com>
2020-05-08 10:56:36 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddNumbers(64, 32);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        private static int AddNumbers(int numberOne, int numberTwo)
+        {
+            int CalculationResult;
+            CalculationResult = numberOne + numberTwo;
+            return CalculationResult;
+        }
+    }
+}
